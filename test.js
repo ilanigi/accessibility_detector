ed7196cfe98255b615a5992c9f541cd39b61aef5
gilad.i <gilad.i@dataloop.ai>
2025-01-18 23:11:22 +0200

    added thrshold handling

--- test.js
+++ test.js
@@ -6,28 +6,67 @@ const fs = require("fs");
   // get all the html files in the tests folder
   const tests = fs.readdirSync(path.join(process.cwd(), "tests"));
   const resultsFolder = `results/test-run-${Date.now()}`;
-    fs.mkdirSync(resultsFolder, { recursive: true });
-  for (const test of tests) {
-    if (!test.endsWith(".html")) {
-      continue;
+  fs.mkdirSync(resultsFolder, { recursive: true });
+  const thresholds = [ 2, 3, 4, 5, 6, 7];
+  const results = [
+    [
+      "test",
+      "threshold",
+      "true positive",
+      "false positive",
+      "false negative",
+      "true negative",
+      "sensitivity",
+      "specificity",
+      "precision",
+      "f1 score",
+    ],
+  ];
+  for (const threshold of thresholds) {
+    const thresholdFolder = resultsFolder + `/threshold-${threshold}`;
+    fs.mkdirSync(thresholdFolder, { recursive: true });
+    for (const test of tests) {
+      if (!test.endsWith(".html")) {
+        continue;
+      }
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      const localPath = path.join("file://", process.cwd(), "tests", test);
+      await page.goto(localPath);
+      await page.addScriptTag({
+        content: `{${runExperiment}}`,
+      });
+      console.log(
+        "########## running experiment: " +
+          test +
+          " with threshold: " +
+          threshold +
+          " ##########"
+      );
+      page.on("console", (msg) => {
+        console.log(msg.text());
+      });
+
+      const { csv, classificationData, scores } = await page.evaluate(
+        (threshold) => {
+          return runExperiment(threshold);
+        }
+      );
+      results.push([test, threshold, ...scores]);
+      fs.writeFileSync(
+        `${thresholdFolder}/${test}-results.csv`,
+        csv.map((row) => row.join(",")).join("\n")
+      );
+      fs.writeFileSync(
+        `${thresholdFolder}/${test}-classification.csv`,
+        classificationData.map((row) => row.join(",")).join("\n")
+      );
+      await browser.close();
     }
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // navigate to the local file "test1.html"
-    const localPath = path.join("file://", process.cwd(), "tests", test);
-    await page.goto(localPath);
-    await page.addScriptTag({
-      content: `{${runExperiment}}`,
-    });
-    console.log("########## running experiment: " + test + " ##########");
-    page.on("console", (msg) => {
-      console.log(msg.text());
-    });
-
-    const {csv, classificationData} = await page.evaluate(() => runExperiment(5));
-    fs.writeFileSync(`${resultsFolder}/${test}-results.csv`, csv.map((row) => row.join(",")).join("\n")); 
-    fs.writeFileSync(`${resultsFolder}/${test}-classification.csv`, classificationData.map((row) => row.join(",")).join("\n"));
-    await browser.close();
   }
+  fs.writeFileSync(
+    `${resultsFolder}/results.csv`,
+    results.map((row) => row.join(",")).join("\n")
+  );
 })();
-
